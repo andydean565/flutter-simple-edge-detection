5640f5042ecc67f90f74ae2934c35eeda2c8d601
Andrew Dean <andrewdean565@gmail.com>
2020-10-23 12:07:38 +0100

    opencv deps

---
+++ ios/opencv2.framework/Versions/A/Headers/UsacParams.h
@@ -0,0 +1,49 @@
+//
+// This file is auto-generated. Please don't modify it!
+//
+#pragma once
+
+#ifdef __cplusplus
+#import "opencv.hpp"
+
+#else
+#define CV_EXPORTS
+#endif
+
+#import <Foundation/Foundation.h>
+
+
+
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+// C++: class UsacParams
+/**
+ * The UsacParams module
+ *
+ * Member of `Calib3d`
+ */
+CV_EXPORTS @interface UsacParams : NSObject
+
+
+#ifdef __cplusplus
+@property(readonly)cv::Ptr<cv::UsacParams> nativePtr;
+#endif
+
+#ifdef __cplusplus
+- (instancetype)initWithNativePtr:(cv::Ptr<cv::UsacParams>)nativePtr;
++ (instancetype)fromNative:(cv::Ptr<cv::UsacParams>)nativePtr;
+#endif
+
+
+#pragma mark - Methods
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+
